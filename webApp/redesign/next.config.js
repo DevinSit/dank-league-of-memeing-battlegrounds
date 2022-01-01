0483250af3a530f4394a02c5ddefcc9169964efe
Devin Sit <devin@devinsit.com>
2022-01-01 14:26:33 -0500

    Brought in svgr to import SVGs as components.

--- webApp/redesign/next.config.js
+++ webApp/redesign/next.config.js
@@ -5,5 +5,22 @@ module.exports = {
     reactStrictMode: true,
     sassOptions: {
         includePaths: [path.join(__dirname, "src", "styles")]
+    },
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: [
+                {
+                    loader: "@svgr/webpack",
+                    options: {
+                        // Disable SVG optimizations since it seems to strip `stopColor`s
+                        // from linear gradients, resulting in colorless SVG graphics.
+                        svgo: false
+                    }
+                }
+            ]
+        });
+
+        return config;
     }
 };
