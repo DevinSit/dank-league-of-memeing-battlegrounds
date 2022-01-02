ed5be78f060999dc40788b1ef2c2445853d2b40e
Devin Sit <devin@devinsit.com>
2022-01-02 00:41:16 -0500

    Modified the root app to hide overflow when playing the game.

--- webApp/redesign/src/pages/_app.tsx
+++ webApp/redesign/src/pages/_app.tsx
@@ -1,7 +1,25 @@
 import type {AppProps} from "next/app";
+import {useRouter} from "next/router";
+import {useEffect} from "react";
 import "styles/global.scss";
 
+// "Game Mode" is just a fancy way of saying that we need to add a class to `body`
+// when playing the game so as to disable overflow.
+const useGameMode = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (router.pathname === "/game") {
+            document.body.classList.add("game-mode");
+        } else {
+            document.body.classList.remove("game-mode");
+        }
+    }, [router.pathname]);
+};
+
 function MyApp({Component, pageProps}: AppProps) {
+    useGameMode();
+
     return <Component {...pageProps} />;
 }
 
