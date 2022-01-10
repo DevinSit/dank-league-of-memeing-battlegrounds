e8206d9ed3964ad69403c5c38f9acf54c6e11472
Devin Sit <devin@devinsit.com>
2022-01-09 20:26:10 -0500

    Removed an errant semi-colon.

--- webApp/redesign/src/pages/_app.tsx
+++ webApp/redesign/src/pages/_app.tsx
@@ -35,7 +35,7 @@ function MyApp({Component, pageProps}: AppProps) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Component {...pageProps} />;
+            <Component {...pageProps} />
             <AppNavigation />
         </>
     );
