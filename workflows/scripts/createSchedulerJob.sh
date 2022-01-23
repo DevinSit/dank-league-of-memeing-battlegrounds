1991b93317a31feb40960852185e502f8e74f110
Devin Sit <devin@devinsit.com>
2022-01-22 22:08:03 -0500

    Changed the scraping job to run every hour.

--- workflows/scripts/createSchedulerJob.sh
+++ workflows/scripts/createSchedulerJob.sh
@@ -9,7 +9,7 @@ gcloud projects add-iam-policy-binding serverless-hackathon-devin \
 
 # Create scheduler job.
 gcloud scheduler jobs create http execute-scrapePostsWorkflows \
-    --schedule="0 4 * * * " \
+    --schedule="0 * * * *" \
     --location="us-east1" \
     --uri="https://workflowexecutions.googleapis.com/v1/projects/serverless-hackathon-devin/locations/us-central1/workflows/scrapePostsWorkflow/executions" \
     --time-zone="America/Toronto" \
