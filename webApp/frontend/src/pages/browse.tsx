3253a98917c62373176178871ccd99ba8d21f6ba
Devin Sit <devin@devinsit.com>
2022-01-23 16:12:19 -0500

    Fixed the development env to use localhost rather than 'backend'; changed the Browse page to use client-side fetching rather than server-side because of this.

--- webApp/frontend/src/pages/browse.tsx
+++ webApp/frontend/src/pages/browse.tsx
@@ -1,32 +1,22 @@
 import type {NextPage} from "next";
 import Head from "next/head";
+import {useSWR} from "hooks/";
 import {Browse} from "scenes/";
 import type {Post} from "types";
 import {api} from "values/api";
 
-interface BrowsePageProps {
-    posts: Array<Post>;
-}
+const BrowsePage: NextPage = () => {
+    const {data} = useSWR<{posts: Array<Post>}>(api.LATEST_MEMES);
 
-const BrowsePage: NextPage<BrowsePageProps> = ({posts = []}) => (
-    <>
-        <Head>
-            <title>Browse | Are you a Dank Memer?</title>
-        </Head>
+    return (
+        <>
+            <Head>
+                <title>Browse | Are you a Dank Memer?</title>
+            </Head>
 
-        <Browse posts={posts} />
-    </>
-);
-
-export async function getServerSideProps() {
-    const response = await fetch(api.LATEST_MEMES);
-    const data: {posts: Array<Post>} = await response.json();
-
-    return {
-        props: {
-            posts: data.posts
-        }
-    };
-}
+            <Browse posts={data?.posts || []} />
+        </>
+    );
+};
 
 export default BrowsePage;
